fea0dbce416c17cc142b6538bf86690305a8179e
amisha1072006 <amishapandey524@gmail.com>
2025-09-10 23:57:47 +0530

    Time: 5 ms (56.33%) | Memory: 44.9 MB (35.51%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,19 +1,45 @@
 class Solution {
     public int romanToInt(String s) {
-       int ans = 0,num=0;
-       for(int i = s.length()-1;i>=0;i--) {
-        switch(s.charAt(i)){
-            case 'I': num = 1;break;
-            case 'V': num = 5;break;
-            case 'X': num = 10;break;
-            case 'L': num = 50;break;
-            case 'C': num = 100;break;
-            case 'D': num = 500;break;
-            case 'M': num = 1000;break;
+    //    int ans = 0,num=0;
+    //    for(int i = s.length()-1;i>=0;i--) {
+    //     switch(s.charAt(i)){
+    //         case 'I': num = 1;break;
+    //         case 'V': num = 5;break;
+    //         case 'X': num = 10;break;
+    //         case 'L': num = 50;break;
+    //         case 'C': num = 100;break;
+    //         case 'D': num = 500;break;
+    //         case 'M': num = 1000;break;
+    //     }
+    //     if(4 * num < ans) ans -=num;
+    //     else ans +=num;
+    //    } 
+    //return ans;
+    //}
+            HashMap<Character,Integer> map=new HashMap<>();
+        map.put('I',1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        int result=0;
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            int value=map.get(s.charAt(i));
+            if(i+1 <n &&value <map.get(s.charAt(i+1)) )
+            {
+                result=result-value;
+            }
+            else
+            {
+                 result=result+value;
+            }
         }
-        if(4 * num < ans) ans -=num;
-        else ans +=num;
-       } 
-       return ans;
+        
+       return result;
     }
 }
