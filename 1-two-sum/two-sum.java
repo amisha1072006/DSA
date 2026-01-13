8ac0138220c48a5fb2cef05d14fbb0f69cff3872
Amisha Pandey <amishapandey524@gmail.com>
2026-01-14 00:39:57 +0530

    Time: 2 ms (99.13%) | Memory: 47.4 MB (12.94%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -6,6 +6,16 @@ class Solution {
     //         return new int[]{i,i+1};
     //     }
     // }return new int[]{};
+    //}
+    // TC - o(n){linear}
+    // SC - O(1) {because we uses fixed size variables ex- n,i and at the end we are returning array in which only two elements are present}
+    // but this is not correct way to  solve this problem because it only check adjacent pairs{nums = [10,2,8,5], target = 15 (this code incorrectly returns an empty array because it checks : 10+2 = 12,2+8 =10,8+5 = 13)}
+
+    //optimal approach
+    //tc-o(n)
+    //sc-o(n){linear space}
+
+    //
     Map<Integer , Integer> numMap = new HashMap<Integer,Integer>();
     int n = nums.length;
     for(int i = 0; i< n; i++){
