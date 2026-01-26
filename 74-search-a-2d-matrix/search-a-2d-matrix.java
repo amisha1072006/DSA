029f72421c22d73a9196ae45f94393716d40a05d
Amisha Pandey <amishapandey524@gmail.com>
2026-01-26 16:23:16 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (5.58%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,13 +1,31 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i <= matrix.length-1;i++){
-            for(int j =0;j<= matrix[0].length-1;j++){
-                if(matrix[i][j]== target){
-                    return true;
-                }
+        //Brute force approach
 
-                }
+        // for(int i=0;i <= matrix.length-1;i++){
+        //     for(int j =0;j<= matrix[0].length-1;j++){
+        //         if(matrix[i][j]== target){
+        //             return true;
+        //         }
+
+        //         }
+        //     }
+        //     return false;
+        // }
+        // OPTMISED APPROACH
+        int col = 0 , row = matrix.length-1;
+        while(col<matrix[0].length && row >=0){
+            if(matrix[row][col] == target){
+                return true;
             }
-            return false;
+            else if(target<matrix[row][col]){
+                row --;
+            }
+            else {
+                col++;
+            }
         }
+        return false;
+        }
+
     }
