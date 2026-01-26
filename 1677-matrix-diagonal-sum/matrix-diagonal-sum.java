b262db1df757726d94d11b2c9eb079fcff2a286c
Amisha Pandey <amishapandey524@gmail.com>
2026-01-26 15:59:03 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (8.14%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -1,13 +1,27 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-        int sum = 0;
-       for(int i = 0;i<mat.length;i++){
-        for(int j = 0;j<mat[0].length;j++){
-           if(i+j == (mat.length-1) || i==j){
-            sum +=mat[i][j];
-           }
+        // BRUTE FORCE APPROACH
+     ///   int sum = 0;
+    //    for(int i = 0;i<mat.length;i++){
+    //     for(int j = 0;j<mat[0].length;j++){
+    //        if(i+j == (mat.length-1) || i==j){
+    //         sum +=mat[i][j];
+    //        }
+    //     }
+    //    } 
+    //    return sum;
+    // }
+
+    //OPTMIZED APPROACH
+     int sum = 0;
+     for(int i=0; i< mat.length;i++){
+        //primary diagnol
+        sum += mat[i][i];
+        //2 nd diagnol 
+        if(i!= mat.length-1-i){
+            sum += mat[i][mat.length-1-i];
         }
-       } 
-       return sum;
+     }
+     return sum;
     }
 }
