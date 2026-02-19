64bbd9371631553fd04901c3cf69c7c74126898e
Amisha Pandey <amishapandey524@gmail.com>
2026-02-19 11:39:00 +0530

    Time: 2 ms (87.80%) | Memory: 44.1 MB (27.00%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -1,36 +1,27 @@
 class Solution {
-    public String removeDuplicateLetters(String t) {
-       char[]s = t.toCharArray();
-        
-        int []visited = new int[26];
-        int[] freq  =new int [26];
-        ArrayDeque<Integer> dq = new ArrayDeque<>();
-        
-        int n=s.length;
-        
-        for(int i=0;i<n;i++){
-            freq[s[i]-'a']++;
-        }
+    public String removeDuplicateLetters(String s) {
+                int len = s.length();
+        boolean pres[] = new boolean[26];
+        int last[] = new int[26];
 
-        for(int i=0;i<n;i++){
-            freq[s[i]-'a']--;
-            if(visited[s[i]-'a']>0)continue;
-            while(!dq.isEmpty()&&s[dq.peekFirst()]>s[i]&&freq[s[dq.peekFirst()]-'a']>0){
-                visited[s[dq.peekFirst()]-'a']=0;
-                dq.poll();
+        for(int i =0;i<26;i++) last[i] = -1;
+        for(int i =0;i<len;i++) last[s.charAt(i) - 97] = i;
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i =0;i<len;i++) {
+            char ch = s.charAt(i);
+            if(pres[ch - 97] == true) continue;
+            while(!stack.isEmpty() && stack.peek() > ch && last[stack.peek() - 97] > i) {
+                pres[stack.peek() - 97] = false;
+                stack.pop();
             }
-            visited[s[i]-'a']=1;
-            dq.offerFirst(i);
+            stack.push(ch);
+            pres[ch-97] = true;
         }
 
-
-
-        StringBuilder sb = new StringBuilder();
-        while(!dq.isEmpty()){
-            sb.append(s[dq.pollLast()]);
-        }
-        return sb.toString();
-        
-    
+        StringBuilder result = new StringBuilder("");
+        while(!stack.isEmpty())result.append(stack.pop());
+        return (result.reverse()).toString();
     }
 }
