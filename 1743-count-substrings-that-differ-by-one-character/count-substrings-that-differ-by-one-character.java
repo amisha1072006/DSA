81f8b4e2ae02c3a9ef4ec6fe2d38710e78dae325
Amisha Pandey <amishapandey524@gmail.com>
2026-04-05 08:07:21 +0530

    Time: 6 ms (91.43%) | Memory: 42.8 MB (61.90%) - LeetSync

---
+++ 1743-count-substrings-that-differ-by-one-character/count-substrings-that-differ-by-one-character.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countSubstrings(String s, String t) {
+        int lenS = s.length();
+        int lenT = t.length();
+        int count = 0;
+        for(int i=0;i<lenS;i++) {
+            for(int j=0;j<lenT;j++) {
+                int diff = 0;
+                for(int m=i,n=j ; m<lenS && n < lenT ; m++,n++) {
+                    if(s.charAt(m) != t.charAt(n)) {
+                        diff++;
+                    }
+                    if(diff == 1) {
+                        count++;
+                    }
+                    if(diff == 2) {
+                        break;
+                    }
+                }
+            }
+        }
+        return count;
+
+    }
+}
