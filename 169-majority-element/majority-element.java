e6a187f61589bb60df27accc47ca4c87be8b1282
Amisha Pandey <amishapandey524@gmail.com>
2026-06-04 23:37:40 +0530

    Time: 10 ms (35.37%) | Memory: 55.5 MB (58.80%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,19 +1,37 @@
+import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
-       int maxCount = 0, maxAns = 0;
-       for(int i = 0; i<nums.length; i++){
-            int ans = 0, count = 1;
-            for(int j = i+1; j<nums.length; j++){
-                if(nums[i] == nums[j]){
-                    ans = nums[i];
-                    count++;
-                }
+        // BRUTE FORCE APPROACH
+
+    //    int maxCount = 0, ans = 0;
+    //    for(int i = 0; i<nums.length; i++){
+    //         int count = 1;
+    //         for(int j = i+1; j<nums.length; j++){
+    //             if(nums[i] == nums[j]){
+    //                 count++;
+    //             }
+    //         }
+    //         if(count > maxCount){
+    //                 maxCount = count;
+    //                 ans = nums[i];
+    //         }
+    //    } 
+    //    return ans;
+
+    // OPTMIZED APPROACH
+
+    Arrays.sort(nums);
+    HashSet<Integer> processed = new HashSet<>();
+    int n = nums.length;
+    for(int i = 0; i<nums.length; i++){
+        int currentElement = nums[i];
+        if(!processed.contains(currentElement)){
+            if(i + n / 2 < n && nums[i+ n / 2] == currentElement){
+                return currentElement;
             }
-                if(count > maxCount){
-                    maxCount = count;
-                    maxAns = nums[i];
-                }
-       } 
-       return maxAns;
+            processed.add(currentElement);
+        } 
     }
+    return -1;
+}
 }
