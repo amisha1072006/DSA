737d1c23eef0e4cf872eb18dea454cbc62c5ad06
Amisha Pandey <amishapandey524@gmail.com>
2026-08-04 00:53:46 +0530

    Time: 2 ms (92.13%) | Memory: 47.7 MB (76.63%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,20 +1,45 @@
 class Solution {
     public void moveZeroes(int[] nums) {
        // Brute Force Approach
-       int n = nums.length;
-       int temp[] = new int[n];
-       int j =0;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] != 0){
-              temp[j++] = nums[i];
-            }
+    //    int n = nums.length;
+    //    int temp[] = new int[n];
+    //    int j =0;
+    //     for(int i = 0; i<nums.length; i++){
+    //         if(nums[i] != 0){
+    //           temp[j++] = nums[i];
+    //         }
+    //     }
+    //     while(j<n){
+    //         temp[j] = 0;
+    //         j++;
+    //     }
+    //     for(int i = 0; i< temp.length; i++){
+    //         nums[i] = temp[i];
+    //     }
+
+    // optmize approach
+
+     int j = -1;
+     for(int i = 0; i<nums.length; i++){
+        if(nums[i] ==0){
+            j =i;
+            break;
         }
-        while(j<n){
-            temp[j] = 0;
+     }
+     if(j == -1){
+        return;
+     }
+     
+     for(int k = j+1; k<nums.length; k++){
+        if(nums[k] != 0){
+            swap(nums, k, j);
             j++;
         }
-        for(int i = 0; i< temp.length; i++){
-            nums[i] = temp[i];
-        }
+     }
     }
+    public static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+     }
 }
