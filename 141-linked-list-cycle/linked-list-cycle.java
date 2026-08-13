18c8fb1a08dbb71d00a1de8eb1c28104593e513d
Amisha Pandey <amishapandey524@gmail.com>
2026-08-13 23:05:03 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (33.60%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,23 +11,15 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-      // Edge case: empty list or single node cannot form a cycle
-        if (head == null || head.next == null) return false;
-
-        ListNode slow = head;       // moves 1 step at a time
-        ListNode fast = head.next;  // moves 2 steps at a time
-
-        while (slow != fast) {
-            // If fast reaches the end, no cycle exists
-            if (fast == null || fast.next == null) {
-                return false;
-            }
-            slow = slow.next;       // move slow pointer by 1
-            fast = fast.next.next;  // move fast pointer by 2
-        }
-
-        // If slow == fast, pointers met inside a cycle
-        return true;
-    
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+         slow = slow.next;
+         fast = fast.next.next;
+         if(slow == fast){
+            return true;
+         }
     }
+    return false;
+}
 }
