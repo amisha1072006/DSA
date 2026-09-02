ef7db2d965ae7c43deed31394a22c23a2dbea9c6
Amisha Pandey <amishapandey524@gmail.com>
2026-09-03 00:49:12 +0530

    Time: 5 ms (1.81%) | Memory: 43.4 MB (12.25%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,29 +1,50 @@
 class Solution {
     public boolean check(int[] nums) {
-        int n = nums.length;
-        int arr[] = new int[nums.length];
-        for(int r=0; r<=n; r++){
-            int idx = 0;
-            for(int i=r; i<n; i++){
-                arr[idx] = nums[i];
-                idx++;
-            }
-            for(int i = 0; i<r; i++){
-                arr[idx] = nums[i];
-                idx++;
-            }
-            // is the array sorted
-            boolean isSort = true;
-            for(int i = 0; i<n-1; i++){
-                if(arr[i] > arr[i+1]){
-                   isSort = false;
-                   break;
-                }
-            }
-            if(isSort == true){
-                return true;
-            }
+        //BRUTE FORCE APPROACH
+        // TC = O(n^2), SC = o(n)
+        // int n = nums.length;
+        // int arr[] = new int[nums.length];
+        // for(int r=0; r<=n; r++){
+        //     int idx = 0;
+        //     for(int i=r; i<n; i++){
+        //         arr[idx] = nums[i];
+        //         idx++;
+        //     }
+        //     for(int i = 0; i<r; i++){
+        //         arr[idx] = nums[i];
+        //         idx++;
+        //     }
+        //     // is the array sorted
+        //     boolean isSort = true;
+        //     for(int i = 0; i<n-1; i++){
+        //         if(arr[i] > arr[i+1]){
+        //            isSort = false;
+        //            break;
+        //         }
+        //     }
+        //     if(isSort == true){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        // Better Approach
+        // TC = O(n^2), SC = O(n)
+       int n = nums.length;
+       int arr[] = Arrays.copyOf(nums, n);
+       Arrays.sort(arr);
+       for(int r=0; r<n; r++){
+        boolean isSorted = true;
+        for(int i=0; i<n; i++){
+          if(arr[i] != nums[(i+r)%n]){
+            isSorted = false;
+            break;
+          }
         }
+        if(isSorted){
+            return true;
+        }
+       }
         return false;
     }
 }
