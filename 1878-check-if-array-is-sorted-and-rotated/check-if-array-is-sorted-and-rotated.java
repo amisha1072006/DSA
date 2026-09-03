7f0cf54cc5df8109e91bc6ba5127d48a9a9d1c99
Amisha Pandey <amishapandey524@gmail.com>
2026-09-04 00:09:03 +0530

    Time: 2 ms (4.83%) | Memory: 43 MB (65.02%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -2,31 +2,31 @@ class Solution {
     public boolean check(int[] nums) {
         //BRUTE FORCE APPROACH
         // TC = O(n^2), SC = o(n)
-        // int n = nums.length;
-        // int arr[] = new int[nums.length];
-        // for(int r=0; r<=n; r++){
-        //     int idx = 0;
-        //     for(int i=r; i<n; i++){
-        //         arr[idx] = nums[i];
-        //         idx++;
-        //     }
-        //     for(int i = 0; i<r; i++){
-        //         arr[idx] = nums[i];
-        //         idx++;
-        //     }
-        //     // is the array sorted
-        //     boolean isSort = true;
-        //     for(int i = 0; i<n-1; i++){
-        //         if(arr[i] > arr[i+1]){
-        //            isSort = false;
-        //            break;
-        //         }
-        //     }
-        //     if(isSort == true){
-        //         return true;
-        //     }
-        // }
-        // return false;
+        int n = nums.length;
+        int arr[] = new int[nums.length];
+        for(int r=0; r<n; r++){
+            int idx = 0;
+            for(int i=r; i<n; i++){
+                arr[idx] = nums[i];
+                idx++;
+            }
+            for(int i = 0; i<r; i++){
+                arr[idx] = nums[i];
+                idx++;
+            }
+            // is the array sorted
+            boolean isSort = true;
+            for(int i = 0; i<n-1; i++){
+                if(arr[i] > arr[i+1]){
+                   isSort = false;
+                   break;
+                }
+            }
+            if(isSort == true){
+                return true;
+            }
+        }
+        return false;
 
         // Better Approach
         // TC = O(n^2), SC = O(n)
@@ -50,17 +50,17 @@ class Solution {
     // OPTIMISE APPROACH
     //TC = O(n), SC = O(n)
 
-    int n = nums.length;
-    int peak = 0;
-    for(int i = 0; i < n; i++){
-        if(nums[i] > nums[(i+1)%n]){
-           peak++;
-        }
-    }
-    if(peak <= 1){
-        return true;
-    } else{
-    return false;
-    }
+    // int n = nums.length;
+    // int peak = 0;
+    // for(int i = 0; i < n; i++){
+    //     if(nums[i] > nums[(i+1)%n]){
+    //        peak++;
+    //     }
+    // }
+    // if(peak <= 1){
+    //     return true;
+    // } else{
+    // return false;
+    // }
     }
 }
