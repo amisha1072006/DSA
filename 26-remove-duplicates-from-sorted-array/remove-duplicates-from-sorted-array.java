8132491d9d176994764618ab2e6c4ccf942b4269
Amisha Pandey <amishapandey524@gmail.com>
2026-09-04 20:08:10 +0530

    Time: 264 ms (3.48%) | Memory: 46.9 MB (24.28%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,18 +1,40 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-       int i = 0;
-        int j = 0;
-        int k = 0;
+    // BRUTE FORCE APPROACH
+    // TC = O(n^2), SC = O(n)
+    int n = nums.length;
+    ArrayList<Integer> list = new ArrayList<>();
+    for(int i = 0; i < n; i++){
+        boolean duplicate = false;
+       for(int j = 0; j < i; j++){
+           if(nums[i] == nums[j]){
+            duplicate = true;
+            break;
+           }  
+       }
+       if(duplicate == false){
+         list.add(nums[i]);
+       }
+    }
+    for(int i =0; i<list.size(); i++){
+        nums[i] = list.get(i);
+    }
+    return list.size();
 
-        while (j < nums.length) {
-            if (nums[i] == nums[j]) {
-                j++;
-            } else {
-                i++;
-                nums[i] = nums[j];
-                k++;
-            }
-        }
-        return k + 1; 
+
+    //    int i = 0;
+    //     int j = 0;
+    //     int k = 0;
+
+    //     while (j < nums.length) {
+    //         if (nums[i] == nums[j]) {
+    //             j++;
+    //         } else {
+    //             i++;
+    //             nums[i] = nums[j];
+    //             k++;
+    //         }
+    //     }
+    //     return k + 1; 
     }
 }
