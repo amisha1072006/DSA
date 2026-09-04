d65e2c9c5f3c01672ac1755c033402c98ba1bf18
Amisha Pandey <amishapandey524@gmail.com>
2026-09-04 23:03:24 +0530

    Time: 17 ms (3.48%) | Memory: 46.8 MB (24.28%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,26 +1,58 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-    // BRUTE FORCE APPROACH
-    // TC = O(n^2), SC = O(n)
-    int n = nums.length;
-    ArrayList<Integer> list = new ArrayList<>();
-    for(int i = 0; i < n; i++){
-        boolean duplicate = false;
-       for(int j = i+1; j < n; j++){
-           if(nums[i] == nums[j]){
-            duplicate = true;
-            break;
-           }  
-       }
-       if(duplicate == false){
-         list.add(nums[i]);
-       }
-    }
-    for(int i =0; i<list.size(); i++){
-        nums[i] = list.get(i);
-    }
-    return list.size();
+    // BRUTE FORCE APPROACH(IF -VE NUMBERS ARE NOT GIVEN)
+    // TC = O(n^2)
+    // int n = nums.length;
+    // for(int i = 0; i< n; i++){
+    //     for(int j = 0; j<i; j++){
+    //         if(nums[i] == nums[j]){
+    //             nums[i] = -1;
+    //             break;
+    //         }
+    //     }
+    // }
+    // int count = 0;
+    // for(int i = 0; i<n; i++){
+    //     if(nums[i] != -1){
+    //         nums[count++] = nums[i];
+    //     }
+    // }
+    // return count;
 
+    // BRUTE FORCE APPROACH(IF -VE NUMBERS ARE GIVEN)
+    // TC = O(n^2), SC = O(n)
+    // int n = nums.length;
+    // ArrayList<Integer> list = new ArrayList<>();
+    // for(int i = 0; i < n; i++){
+    //     boolean duplicate = false;
+    //    for(int j = i+1; j < n; j++){ // or j = 0 to j< i
+    //        if(nums[i] == nums[j]){
+    //         duplicate = true;
+    //         break;
+    //        }  
+    //    }
+    //    if(duplicate == false){ // or if(!duplicate){
+    //      list.add(nums[i]);
+    //    }
+    // }
+    // for(int i =0; i<list.size(); i++){
+    //     nums[i] = list.get(i);
+    // }
+    // return list.size();
+
+   //BETTER APPROACH
+   //TC = O(n), SC = O(n)
+   int n = nums.length;
+   ArrayList<Integer> list = new ArrayList<>();
+   for(int i = 0; i<n; i++){
+    if(!list.contains(nums[i])){
+        list.add(nums[i]);
+    }
+   }
+   for(int i = 0; i< list.size(); i++){
+    nums[i] = list.get(i);
+   }
+   return list.size();
 
     //    int i = 0;
     //     int j = 0;
